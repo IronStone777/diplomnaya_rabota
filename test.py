e365b6db391d79bd1b7a7b7a5cf029ad13bebddd
IronStone777 <IronStone777@yandex.ru>
2024-03-18 15:35:28 +0300

    My first commit

--- test.py
+++ test.py
@@ -5,9 +5,6 @@ import request
 def test_order_creation():
     creation_response = request.post_new_order()
     track_id = creation_response.json().get('track')
-    if track_id is None:
-        print("Ключ 'track' отсутствует в ответе")
-        return
     response = request.get_orders_track(track_id)
     assert response.status_code == 200
     print(response.json())
