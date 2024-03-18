830b421474930c9dee4d5d3c780c75263bbfed39
IronStone777 <IronStone777@yandex.ru>
2024-03-18 15:49:12 +0300

    My first commit

--- request.py
+++ request.py
@@ -5,10 +5,10 @@ import data
 
 
 def post_new_order(): # запрос на создание нового заказа
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_NEW_ORDER,  # подставялем полный url
-                         json=data.order_body)  # а здесь тело
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_NEW_ORDER,  # подставляем полный url
+                         json=data.order_body)  # здесь тело
 
 
 
-def get_orders_track(track_id): # запрос на получение заказа по его трек номеру
+def get_orders_track(track_id): # запрос на получение заказа по его трек-номеру
     return requests.get(configuration.URL_SERVICE + configuration.RECEIVING_ORDER_BY_ID + "?t=" + str(track_id))
--- test.py
+++ test.py
@@ -6,5 +6,5 @@ def test_order_creation():
     creation_response = request.post_new_order()
     track_id = creation_response.json().get('track')
     response = request.get_orders_track(track_id)
-    assert response.status_code == 200
+    assert response.status_code == 200, f"Expected status_code {200}, but got {response.status_code}"
     print(response.json())
