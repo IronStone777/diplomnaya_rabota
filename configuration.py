70acb5607fceb77e05bda6e1e768eb5f537b47bf
IronStone777 <IronStone777@yandex.ru>
2024-03-18 15:14:54 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://902711a4-29f0-4b79-b3b3-f644608773bf.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Sherlock",
+    "lastName": "Holmes",
+    "address": "Baker Street, 221B",
+    "metroStation": 3,
+    "phone": "+7 800 555 35 35",
+    "rentTime": 4,
+    "deliveryDate": "2024-04-05",
+    "comment": "Elementary, Watson",
+    "color": [
+        "BLACK"
+    ]
+}
