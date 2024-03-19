a0b36fc26663f19e93f4270cdaca0fedfd10dc51
IronStone777 <IronStone777@yandex.ru>
2024-03-19 21:21:12 +0300

    Финальные исправления

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import request
 
-
+# Кондратьев Артем, 14-я когорта - Финальный проект. Инженер по тестированию плюс
 # Тест проверки создания заказа
 def test_order_creation():
     creation_response = request.post_new_order()
     track_id = creation_response.json().get('track')
     response = request.get_orders_track(track_id)
     assert response.status_code == 200, f"Expected status_code {200}, but got {response.status_code}"
-    print(response.json())
+    print("Тест пройден")
